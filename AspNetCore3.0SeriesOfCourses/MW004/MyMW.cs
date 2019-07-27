b8ee20c258a347ac3a0ed40381d0d44ee77dbe9b
桂素伟 <axzxs2001@163.com>
2019-07-27 17:54:31 +0900

    NW004 complete

---
+++ AspNetCore3.0SeriesOfCourses/MW004/MyMW.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MW004
+{
+    public class MyMW
+    {
+        private readonly RequestDelegate _next;
+        public MyMW(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //context.Request
+            await context.Response.WriteAsync("user error");
+            //await _next(context);
+
+        }
+    }
+
+    public static class MyMWExtensions
+    {
+        public static IApplicationBuilder UserMyMW(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MyMW>();
+        }
+    }
+}
