fce61e755326f66362d4aa75e137e8008a8c60f8
桂素伟 <axzxs2001@163.com>
2019-07-27 12:36:54 +0900

    DI00 完成

---
+++ AspNetCore3.0SeriesOfCourses/DI002/IMyService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DI002
+{
+    public interface IMyService
+    {
+        void Print(string method);
+    }
+
+    public class MyService : IMyService
+    {
+        public void Print(string method)
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+    public class MyService1 : IMyService
+    {
+        int printCount;
+
+        public void Print(string method)
+        {
+            printCount++;
+            Console.WriteLine($"----{printCount} {method}  {DateTime.UtcNow}");
+        }
+    }
+
+    public interface IYourService
+    {
+        void Print();
+    }
+    public class YourService : IYourService
+    {
+
+        private readonly IMyService _myService;
+        public YourService(IMyService myService)
+        {
+            _myService = myService;
+        }
+        public void Print()
+        {
+            _myService.Print("YourService.Print");
+        }
+    }
+}
