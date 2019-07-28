fde160e7b16f0ec8e489673296ca39fa87a66932
桂素伟 <axzxs2001@163.com>
2019-07-28 18:14:42 +0900

    011NLog

---
+++ AspNetCore3.0SeriesOfCourses/NLog010/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace NLog010.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+    
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            using (_logger.BeginScope("===批量输出"))
+            {
+                _logger.LogTrace("    Trace");
+                _logger.LogCritical("    Critical");
+                _logger.LogDebug("    Debug");
+                _logger.LogWarning("    Warning");
+                _logger.LogInformation("    Information");
+                _logger.LogError("    Error");
+            }
+
+            return "Ok";
+        }
+    }
+}
---
+++ AspNetCore3.0SeriesOfCourses/NLog010/Program.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+
+namespace NLog010
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logger = NLog.Web.NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+            try
+            {
+                logger.Info("启动NLog010项目");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exc)
+            {
+                logger.Fatal(exc, $"启动NLog010项目异常：{exc.Message}");
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+            .UseNLog();
+    }
+}
