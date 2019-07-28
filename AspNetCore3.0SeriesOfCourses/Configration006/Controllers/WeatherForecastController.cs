ebc39ff3e3a969c0f6b8c0996243c5665bd7f2b5
桂素伟 <axzxs2001@163.com>
2019-07-28 10:00:44 +0900

    Configuration006

---
+++ AspNetCore3.0SeriesOfCourses/Configration006/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Configration006.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly Appsetting _appsetting;
+        public WeatherForecastController(IConfiguration configuration, ILogger<WeatherForecastController> logger, IOptionsSnapshot<Appsetting> options)
+        {
+            _appsetting = options.Value;
+            var key = configuration.GetSection("Appsetting:key");
+        
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return _appsetting.Key;
+        }
+    }
+}
