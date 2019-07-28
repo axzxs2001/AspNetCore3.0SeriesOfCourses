6f55265873fc9ec3f33979db9292258d062a1eb0
桂素伟 <axzxs2001@163.com>
2019-07-28 17:34:18 +0900

    Log009

---
+++ AspNetCore3.0SeriesOfCourses/Log009/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Log009.LogTest;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Log009.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogTestService _logTestService;
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, ILogTestService logTestService)
+        {
+            _logTestService = logTestService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            _logTestService.Log();
+            using(_logger.BeginScope("===批量输出"))
+            {
+                _logger.LogTrace("    Trace");
+                _logger.LogCritical("    Critical");
+                _logger.LogDebug("    Debug");
+                _logger.LogWarning("    Warning");
+                _logger.LogInformation("    Information");
+                _logger.LogError("    Error");
+            }
+            return "Ok";
+        }
+    }
+}
---
+++ AspNetCore3.0SeriesOfCourses/Log009/ILogTestService.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Log009.LogTest
+{
+    public interface ILogTestService
+    {
+        void Log();
+    }
+    public class LogTestService : ILogTestService
+    {
+        private readonly ILogger<LogTestService> _logger;
+
+        public LogTestService(ILogger<LogTestService> logger)
+        {
+            _logger = logger;
+        }
+        public void Log()
+        {
+            _logger.LogTrace("-----LogTestService   Trace");
+            _logger.LogCritical("-----LogTestService    Critical");
+            _logger.LogDebug("-----LogTestService    Debug");
+            _logger.LogWarning("-----LogTestService    Warning");
+            _logger.LogInformation("-----LogTestService    Information");
+            _logger.LogError("-----LogTestService    Error");
+        }
+    }
+}
